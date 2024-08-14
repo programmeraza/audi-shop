a8255d39ac53035139ca4b86db151014fae5a590
Aziz <157486847+programmeraza@users.noreply.github.com>
2024-08-14 22:41:05 +0500

    feat: one page v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import './App.scss';
-import HomePages from './pages/HomePages';
-import LoginPages from './pages/LoginPages';
-import ErrorPages from './pages/ErrorPages';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import SwiperComponent from './components/SwiperComponents/SwiperComponents';
+import Shop from './components/Shop/Shop';
+import Reser from './components/Reser/Reser';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePages />} />
-        <Route path='/login' element={<LoginPages />} />
-        <Route path='*' element={<ErrorPages />} />
-      </Routes>
-    </Router>
+    <>
+    <Header/>
+    <Hero/>
+    <SwiperComponent/>
+    <Shop/>
+    <Reser/>
+    </>
   );
 }
 
